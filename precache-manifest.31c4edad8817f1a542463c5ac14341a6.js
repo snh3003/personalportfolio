abde2de460650ab355e6733e3e60173e6aaeac16
Shahab Hashmi <shahsama542@gmail.com>
2021-01-27 01:22:02 +0530

    Updates

---
+++ precache-manifest.31c4edad8817f1a542463c5ac14341a6.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fd697ed8efd29f0af17e6734f8dc96a",
+    "url": "/personalportfolio/index.html"
+  },
+  {
+    "revision": "b8fedbe0a39613b54b8f",
+    "url": "/personalportfolio/static/css/3.9998556b.chunk.css"
+  },
+  {
+    "revision": "0b79c1bc0e29ef893bb8",
+    "url": "/personalportfolio/static/css/4.01c87591.chunk.css"
+  },
+  {
+    "revision": "307bee78cb73f01624d3",
+    "url": "/personalportfolio/static/css/main.a35e6043.chunk.css"
+  },
+  {
+    "revision": "77fc91bba609ad121162",
+    "url": "/personalportfolio/static/js/2.fd15acf3.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/personalportfolio/static/js/2.fd15acf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8fedbe0a39613b54b8f",
+    "url": "/personalportfolio/static/js/3.05536770.chunk.js"
+  },
+  {
+    "revision": "0b79c1bc0e29ef893bb8",
+    "url": "/personalportfolio/static/js/4.448c991e.chunk.js"
+  },
+  {
+    "revision": "307bee78cb73f01624d3",
+    "url": "/personalportfolio/static/js/main.aef4d8fb.chunk.js"
+  },
+  {
+    "revision": "8a527ba421e00f189e3a",
+    "url": "/personalportfolio/static/js/runtime-main.f641620d.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/personalportfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/personalportfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/personalportfolio/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/personalportfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/personalportfolio/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/personalportfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "5b108e56bb58666d697ebdd87985e791",
+    "url": "/personalportfolio/static/media/minerva.5b108e56.png"
+  },
+  {
+    "revision": "2636b59fe6d7fb21160cf9a93e8ae7e3",
+    "url": "/personalportfolio/static/media/quantumhack.2636b59f.jpg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/personalportfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "12f94291e04a1feade3ddc1bb5152c43",
+    "url": "/personalportfolio/static/media/socialfeathers.12f94291.png"
+  },
+  {
+    "revision": "39eff4a91b01e00a32cb78f3797eec88",
+    "url": "/personalportfolio/static/media/talksCardBack.39eff4a9.svg"
+  }
+]);
